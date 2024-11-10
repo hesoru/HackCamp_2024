d3372d5b5a007b184d4eef6cb8481805cacec126
hesoru <95680931+hesoru@users.noreply.github.com>
2024-11-10 14:14:09 -0800

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-printf("Hello, Ore's Commit")
-printf("Hello, Helena's Commit")
-
-# check branch
-git branch
-# go to your branch
-git checkout Helena
-# push
-git push --set-upstream origin [branch]
